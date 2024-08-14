4337e188214912657b53b6c7b4e27c7dc27921a5
Lennon Benedict D. Jansuy <45531522+lenn0n@users.noreply.github.com>
2024-08-14 12:28:14 +0800

    Update usage.tsx

--- frontend/hooks/useFetch/usage.tsx
+++ frontend/hooks/useFetch/usage.tsx
@@ -13,8 +13,8 @@ function Page({ error, data, message }: any) {
     >
       <div className="">
         {error && message}
-        {data?.clients?.length > 0 &&
-          data.clients.map(
+        {data?.length > 0 &&
+          data.map(
             (client: ClientProps) =>
               <div key={client.client_id}>{client.client_name}</div>
           )
@@ -27,24 +27,25 @@ function Page({ error, data, message }: any) {
 export const getServerSideProps = async () => {
   const { request } = useFetch({
     url: 'http://localhost:5000/graphql',
+    // url: 'http://localhost:5000/api/client',
     cache: 'no-cache',
     method: 'GET',
     isGraphQL: true
   })
 
   // Using GRAPHQL
-  // const data = await request(`
-  //   query getClients { 
-  //     clients { 
-  //       client_name 
-  //       client_id
-  //     } 
-  //   }`)
+  const data = await request(`
+    query getClients { 
+      clients { 
+        client_name 
+        client_id
+      } 
+    }`)
 
   // USING REST
-  const data = await request({ client_id: 1})
-
-  return data;
+  // const data = await request({ client_id: 1})
+  
+  return { props: data };
 };
 
 
